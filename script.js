fe95dac9875ab35c41d0557c2bbf3041897b73fc
Ivan Torres <ivantorresmia27@gmail.com>
2020-09-26 16:25:33 -0700

    local storage and color changes

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
 $(document).ready(function () {
   $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
 
-  // Hool for the text area
 
-  // create a hook for for the value of the time
   var timeValue = $(".description");
-  //console.log(timeValue);
-
-  // Hook for the current time
+ 
   var currentTime = moment().format("H");
-  //console.log(currentTime);
-
-  // if (moment(timeValue).isBefore(currentTime)) {
-
-  // }
 
   for (i = 0; i < timeValue.length; i++) {
     var hour = timeValue[i].getAttribute("value");
-    if (hour === currentTime) {
+    if (hour == currentTime) {
       timeValue[i].classList.add("present");
-    } 
-    if (hour < currentTime) {
+    } else if (hour < currentTime) {
         timeValue[i].classList.add("past");
-    } 
-    if(hour > currentTime) {
+    } else {
         timeValue[i].classList.add("future");
     }
 
@@ -33,21 +22,21 @@ $(document).ready(function () {
     var timeBlock = $(this).siblings("textarea")[0];
     var task = timeBlock.value;
     var hour = timeBlock.getAttribute("value");
-    // console.log(hour);
-    // console.log(task);
     localStorage.setItem(hour, task);
   });
 
   for (i = 9; i < 18; i++) {
     var task = localStorage.getItem(i.toString());
-    // console.log(task);
     //jqeury
-    $("[value="+i.toString()+"]");
-    if (task === null) {
-    }
+   var value = $("[value="+i.toString()+"]");
+   
+   if (task === null) {
+       value.text("")
+   } else {
+       value.text(task);
+   }
+
+
   }
 
 });
-
-//javascript
-console.log(document.querySelector(("[value='9']")));
