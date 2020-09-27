cd4b404b5ea89b93178181ecce127c0a30014e13
Ivan Torres <ivantorresmia27@gmail.com>
2020-09-26 17:32:40 -0700

    clean up and comments

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+
+
+// This writes the current day, month and year to the currentDay Id.
 $(document).ready(function () {
   $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
 
-
+// These are tags that I will be using to compare times and add classes.
   var timeValue = $(".description");
- 
   var currentTime = moment().format("H");
 
+// This code uses a for loop to run through all of the time blocks, taking the description class to reach the value of it
+// Then it compares times and depending on the time it will add classes already written in style.css
   for (i = 0; i < timeValue.length; i++) {
     var hour = timeValue[i].getAttribute("value");
     if (hour == currentTime) {
@@ -18,6 +22,9 @@ $(document).ready(function () {
 
   }
 
+
+// This ode was the funnest/hardest for me. I reached the text area's value by using .siblings
+// Then I set all the values of text area to the local storage. 
   $(".saveBtn").on("click", function () {
     var timeBlock = $(this).siblings("textarea")[0];
     var task = timeBlock.value;
@@ -25,9 +32,10 @@ $(document).ready(function () {
     localStorage.setItem(hour, task);
   });
 
+//   This for loop reaches every key in the local storage by using index and incrementing 
+//  then once retrieving it, I add the value of it to the text areas, if it returns null then it returns an empty string. 
   for (i = 9; i < 18; i++) {
     var task = localStorage.getItem(i.toString());
-    //jqeury
    var value = $("[value="+i.toString()+"]");
    
    if (task === null) {
